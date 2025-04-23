5456002f726759c81d9cdd95010f4f0bbf693d9c
bloodsbro <vanka2929@gmail.com>
2025-04-23 19:02:02 +0300

    Added basePath

--- zudoku.config.ts
+++ zudoku.config.ts
@@ -1,6 +1,7 @@
 import type { ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
+  basePath: "/prof_english_3",
   topNavigation: [
     { id: "docs", label: "Documentation" },
     { id: "api", label: "API Reference" },
