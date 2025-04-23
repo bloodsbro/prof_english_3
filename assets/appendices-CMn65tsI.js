54a978f82357c149f8679a6cee47dbdd1f7d1e43
bloodsbro <vanka2929@gmail.com>
2025-04-23 18:59:39 +0300

    Deploy Zudoku docs

---
+++ assets/appendices-CMn65tsI.js
@@ -0,0 +1,6 @@
+import{H as i,j as n}from"./entry.client-CfQHZ0b1.js";const c=[{depth:1,value:"4. Appendices",id:"4-appendices"}],a=void 0;function s(t){const e={a:"a",h1:"h1",li:"li",ul:"ul",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(e.h1,{id:"4-appendices",children:"4. Appendices"}),`
+`,n.jsxs(e.ul,{children:[`
+`,n.jsxs(e.li,{children:["Example API documentation: ",n.jsx(e.a,{href:"/openapi.yaml",children:"OpenAPI specification"}),"."]}),`
+`,n.jsxs(e.li,{children:["Example usage of documentation in Zudoku: ",n.jsx(e.a,{href:"https://zudoku.dev/",children:"Zudoku Demo"}),"."]}),`
+`]})]})}function d(t={}){const{wrapper:e}={...i(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(s,{...t})}):s(t)}export{d as default,a as frontmatter,c as tableOfContents};
+//# sourceMappingURL=appendices-CMn65tsI.js.map
